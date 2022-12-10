596916a8488a5c793e21831be99c2d7720d04eeb
Marpond <sonkoly.marci02@gmail.com>
2022-12-10 16:19:01 +0100

    Simplified the 2FA message.

--- WeShare/Models/Security/EmailHandler.cs
+++ WeShare/Models/Security/EmailHandler.cs
@@ -11,34 +11,17 @@ public class EmailHandler
 
     public EmailHandler(string receiver)
     {
-        // Get the project path, go up by three directories to get to the root of the project
         ProjectPath = Directory.GetCurrentDirectory();
         // Get the email address and password from email.json at the root of the project
         var json = File.ReadAllText(Path.Combine(ProjectPath, "email.json"));
         _account = JsonConvert.DeserializeObject<Account>(json)!;
         Receiver = receiver;
         Code = new Random().Next(100000, 999999);
-        IpAddress = GetIp().Result;
-        Location = GetLocation().Result;
     }
 
     public int Code { get; }
     private string Receiver { get; }
-    private string IpAddress { get; }
-    private JObject Location { get; }
     private string ProjectPath { get; }
-
-    private async Task<string> GetIp()
-    {
-        var externalIp = await new HttpClient().GetStringAsync("http://icanhazip.com");
-        return externalIp;
-    }
-
-    private async Task<JObject> GetLocation()
-    {
-        var location = await new HttpClient().GetStringAsync("http://ip-api.com/json/" + IpAddress);
-        return JObject.Parse(location);
-    }
 
     public void SendSessionKey()
     {
@@ -47,10 +30,9 @@ public class EmailHandler
         mail.From = new MailAddress(_account.Email);
         mail.To.Add(Receiver);
         mail.Subject = "Two Factor Authentication";
-        var body = @$"There was a new login attempt to your account.
-Ip: {IpAddress}Location: {Location["zip"]} {Location["city"]}, {Location["country"]}, {Location["regionName"]}
-At: {DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")}.
-If this was you, please enter the following code: {Code}";
+        var body = @$"There was a new login attempt to your account at {DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")}.
+If this wasn't you, change your password immediately.
+Otherwise, enter the following code: {Code}";
         mail.Body = body;
         smtpServer.Port = 587;
         smtpServer.Credentials = new NetworkCredential($"{_account.Email}", $"{_account.Password}");
